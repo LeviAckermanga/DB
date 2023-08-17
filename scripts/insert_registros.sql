81a3e4ee986f47aad275e592878d1ad0b60dc516
LeviAckerman <90481634+LeviAckermanga@users.noreply.github.com>
2023-08-17 12:42:20 -0400

    Add files via upload

---
+++ scripts/insert_registros.sql
@@ -0,0 +1,6 @@
+-- insert_registros.sql
+
+INSERT INTO registros (nombre, edad, email) VALUES ('Usuario1', 25, 'usuario1@example.com');
+INSERT INTO registros (nombre, edad, email) VALUES ('Usuario2', 30, 'usuario2@example.com');
+INSERT INTO registros (nombre, edad, email) VALUES ('Usuario3', 22, 'usuario3@example.com');
+-- Agrega más sentencias INSERT según sea necesario
